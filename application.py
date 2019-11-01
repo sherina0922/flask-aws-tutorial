8c6b1c4bd1f17112d47b7538b95936b14175faaa
Sherina Hung <sherina0922@gmail.com>
2019-10-31 21:31:14 -0500

    realizing that cant delete

--- application.py
+++ application.py
@@ -10,7 +10,9 @@ Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-
 from flask import Flask, render_template, request
 from application import db
 from application.models import User
-from application.forms import EnterDBInfo, RetrieveDBInfo
+from application.forms import EnterDBInfo, RetrieveDBInfo, DeleteDBInfo, UpdateDBInfo
+#import logging
+#from sqlachemy.exc import IntegrityError
 
 # Elastic Beanstalk initalization
 application = Flask(__name__)
@@ -23,6 +25,7 @@ application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 def index():
     form1 = EnterDBInfo(request.form)
     form2 = RetrieveDBInfo(request.form)
+    # form3 = DeleteDBInfo(request.form)
 
     if request.method == 'POST' and form1.validate():
         userInfoDict = dict(item.split("=") for item in form1.dbInfo.data.split(";"))
@@ -31,24 +34,41 @@ def index():
             db.session.add(data_entered)
             db.session.commit()
             db.session.close()
+        # except IntegrityError as e:
+        #     reason = e.message
+        #     logging.warning(reason)
         except:
             db.session.rollback()
         return render_template('thanks.html',username = userInfoDict.get("username"), password=userInfoDict.get("password"), name=userInfoDict.get("name"), gender=userInfoDict.get("gender"), budget=userInfoDict.get("budget"), weight=userInfoDict.get("weight"), weight_goal=userInfoDict.get("weight_goal"), avg_cals_burned=userInfoDict.get("avg_cals_burned"), location=userInfoDict.get("location"), notes=userInfoDict.get("notes")) #change this html
 
     if request.method == 'POST' and form2.validate():
         query_db = None
-        username_return = form2.userRetrieve.data
-        query_db = User.query.filter_by(username=username_return).one()
         try:
-            # user_id_return = form2.userRetrieve.data
-            # query_db = Data.query.filter_by(user_id=user_id_return).one()
+            username_return = form2.userRetrieve.data
+            query_db = User.query.filter_by(username=username_return).one()
+            if (query_db is None):
+                db.session.close()
+                return render_template('noresult.html', username_return=username_return)
             db.session.close()
         except:
-            username_return = "failed"
             db.session.rollback()
         return render_template('results.html', results=query_db, username_return=username_return)
 
-    return render_template('index.html', form1=form1, form2=form2)
+    # if request.method == 'POST' and form3.validate():
+    #     # query_db = None
+    #     username_return = form3.userRetrieve.data
+    #     # query_db = User.query.filter_by(username=username_return).one().delete()
+    #     User.query.filter_by(username=username_return).delete()
+    #     try:
+    #         # username_return = form3.userRetrieve.data
+    #         # query_db = User.query.filter_by(username=username_return).one().delete()
+    #         db.session.commit()
+    #         db.session.close()
+    #     except:
+    #         db.session.rollback()
+    #     return render_template('goodbye.html', username_return=username_return)
+
+    return render_template('index.html', form1=form1, form2=form2)#,form3=form3)
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
--- application/forms.py
+++ application/forms.py
@@ -5,4 +5,10 @@ class EnterDBInfo(Form):
     dbInfo = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=1024, message=u'Enter 1024 characters or less')])
 
 class RetrieveDBInfo(Form):
-    userRetrieve = TextField(label='Username of user info to get', description="db_get", validators=[validators.required(), validators.Length(min=0, max=256, message=u'Enter the username of the desired user\'s information')])
+    userRetrieve = TextField(label='Username of user info to get', description="db_get", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter the username of the desired user\'s information')])
+
+class DeleteDBInfo(Form):
+    userRetrieve = TextField(label='Username of user to delete from system', description="db_delete", validators=[validators.required(), validators.Length(min=0, max=1024, message=u'Enter the username of user who\'s information to delete')])
+
+class UpdateDBInfo(Form):
+    userRetrieve = TextField(label='Username of user to update information', description="db_update", validators=[validators.required(), validators.Length(min=0, max=1024, message=u'Enter the username of user who\'s information to update and update field: new value')])
